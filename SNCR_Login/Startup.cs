65968673f2f52ede55f9b5e9514e32e5af78eef4
Reain <Einar.Acuna@gmail.com>
2016-10-08 15:05:57 -0400

    Admin Login

---
+++ SNCR_Login/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SNCR_Login.Startup))]
+namespace SNCR_Login
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
